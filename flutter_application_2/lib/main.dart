c68de1d1dd98433dea496e8f00dad535a3d77fd2
OnsChebi <chebbions7@gmail.com>
2024-09-21 12:43:17 +0100

    add text field

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -7,10 +7,31 @@ void  main(){
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ToDoWidget(),
+    );
+  }
+}
+class ToDoWidget extends StatefulWidget {
+  const ToDoWidget({super.key});
+
+  @override
+  State<StatefulWidget> createState() => ToDo();
+}
+
+class ToDo extends State<ToDoWidget> {
+  final TextEditingController taskController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
           //leading: const Icon(Icons.favorite),//to put the icon on the right of the navbar (menu)
           // title: const Text("My NoteBook",
@@ -22,24 +43,29 @@ class MyApp extends StatelessWidget {
           //     child: Icon(Icons.favorite),)
           // ],
 
-          title: Row(
+          title: const Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const Text("My NoteBook",
+              Text("My NoteBook",
               style: TextStyle(color: Color.fromARGB(255,255, 255, 255)),),
-              const Icon(Icons.favorite, color: Color.fromARGB(255,255,255,255),)
+              Icon(Icons.favorite, color: Color.fromARGB(255,255,255,255),)
 
             ],
             ),
             backgroundColor: Color.fromARGB(255,99, 126, 185),
-            
         ),
-        body: const Center (
-          child:Column(
-
-          )),
-      )
-
+        body: Padding(padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: taskController,
+              decoration:const InputDecoration(
+                labelText: "what's your plan for today",
+                border: OutlineInputBorder()
+              ),
+            )
+          ],
+        ),),
     );
   }
 }
